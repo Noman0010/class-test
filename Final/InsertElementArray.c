63f5b205ea5639380dd5d3cb32278f5bd237cfdf
Noman <111453298+Noman0010@users.noreply.github.com>
2023-05-02 08:07:33 +0600

    Add files via upload

---
+++ Final/InsertElementArray.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+
+    int size;
+    scanf("%d",&size);
+    int arr[size+1];
+
+    for(int i=0; i<size; i++){
+        scanf("%d",&arr[i]);
+    }
+    int position,value;
+    scanf("%d%d",&position,&value);
+
+    for(int i=size; i>=position; i--){
+        arr[i]=arr[i-1];
+    }
+    arr[position]=value;
+    
+    for(int i=0; i<=size; i++){
+        printf("%d ",arr[i]);
+    }
+
+
+
+    return 0;
+}
